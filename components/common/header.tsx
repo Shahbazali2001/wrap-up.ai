72e8ba7d3196a6c6133254012db49aa310775c3a
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-11-21 00:42:18 +0500

    working on header

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import { FileText } from "lucide-react";
 
 export default function Header() {
   return (
-    <nav className="container">
-      <div>
-        <Link href="/">WrapUp.ai</Link>
+    <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
+      <div className="flex items-center">
+        <Link className="flex items-center gap-1 lg:gap-2 shrink-0" href="/">
+          <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
+          <span className="text-xl font-extrabold">WrapUp.ai</span>
+        </Link>
       </div>
 
       <div>
         <Link href="/#pricing">Pricing</Link>
+      </div>
+
+      <div>
+        <Link href="/sign-in">Sign In</Link>
       </div>
     </nav>
   );
