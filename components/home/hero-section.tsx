8d50581219939f1c5f147108380bdf1fa8f94d62
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-11-27 22:49:06 +0500

    updated hero section

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -20,7 +20,8 @@ export default function HeroSection() {
         </div>
 
         <h1 className="font-bold py-6 text-center">
-          Transform PDFs into Powerful Summaries that Speaks Out
+          Transform PDFs into Powerful{" "}
+          <span className="text-rose-500">Summaries</span> that Speaks Out
         </h1>
         <h2 className="text-lg sm:text-xl lg:text-2xl text-center px-4 text-gray-600">
           Get a summary of your PDF in seconds
@@ -28,7 +29,7 @@ export default function HeroSection() {
         <div className="flex items-center justify-center py-6">
           <Button
             variant={"link"}
-            className="text-white mt-4 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900  "
+            className="text-white mt-4 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-12 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg transition-all duration-500 ease-in-out transform hover:scale-105"
           >
             <Link href="#/pricing" className="flex gap-2 items-center">
               <span className="">Get Started</span>
