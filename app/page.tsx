41d42c2bf89ea8ec8dc9618f0605ce783960d7ad
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-12-03 01:19:48 +0500

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home/hero-section";
 
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
+import HowItWorks from "@/components/home/how-it-works";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
+        <HowItWorks />
       </div>
     </div>
   );
