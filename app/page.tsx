814230f3ca494bebd83e54ed6d34688c72124580
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-12-12 00:27:23 +0500

    adding pricing section on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
 import HowItWorks from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroSection />
         <DemoSection />
         <HowItWorks />
+        <PricingSection />
       </div>
     </div>
   );
