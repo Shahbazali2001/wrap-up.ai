66d6fdf59e991f33d6410a216b804d6d08c01eb3
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-12-12 00:27:59 +0500

    working on pricing section

---
+++ components/home/pricing-section.tsx
@@ -0,0 +1,17 @@
+export default function PricingSection() {
+  return (
+    <section>
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        {/* Heading */}
+        <div>
+          <h2>Pricing </h2>
+        </div>
+
+        {/* Pricing Cards */}
+        <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
+          Pricing Card
+        </div>
+      </div>
+    </section>
+  );
+}
