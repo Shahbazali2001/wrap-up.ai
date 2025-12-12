a80da114d8669ce139fcd70ff11b6ee64257c043
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2025-12-12 23:58:39 +0500

    working on pricing card

--- components/home/pricing-card.tsx
+++ components/home/pricing-card.tsx
@@ -1,3 +1,32 @@
-export default function PricingCard() {
-  return <div>pricing card</div>;
+import { Plan } from "@/misc/plans";
+import Link from "next/link";
+export default function PricingCard({
+  name,
+  paymentId,
+  description,
+  price,
+  items,
+}: Plan) {
+  return (
+    <div className="relative w-full max-w-lg">
+      <div>
+        <p>{name}</p>
+        <p>{description}</p>
+      </div>
+
+      <div>
+        <p>{price}</p>
+      </div>
+
+      <div>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </div>
+
+      <div>
+        <Link href={`/${paymentId}`}>Buy Now</Link>
+      </div>
+    </div>
+  );
 }
