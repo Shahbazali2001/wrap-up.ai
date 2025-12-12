edde1ee20855e797a5a489b9ddb6536b5b682fa3
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2025-12-13 00:15:36 +0500

    pricing cards coming soon

--- components/home/pricing-card.tsx
+++ components/home/pricing-card.tsx
@@ -9,23 +9,27 @@ export default function PricingCard({
 }: Plan) {
   return (
     <div className="relative w-full max-w-lg">
-      <div>
-        <p>{name}</p>
-        <p>{description}</p>
-      </div>
+      <div className="relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 border-2 border-gray-500/20 rounded-xl">
+        <div className="flex justify-between items-center gap-4">
+          <div>
+            <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+            <p className="text-base-content/80 mt-2">{description}</p>
+          </div>
+        </div>
 
-      <div>
-        <p>{price}</p>
-      </div>
+        <div>
+          <p>$ {price}</p>
+        </div>
 
-      <div>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </div>
+        <div>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </div>
 
-      <div>
-        <Link href={`/${paymentId}`}>Buy Now</Link>
+        <div>
+          <Link href={`/${paymentId}`}>Buy Now</Link>
+        </div>
       </div>
     </div>
   );
--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,3 +1,6 @@
+import PricingCard from "./pricing-card";
+import plans from "@/misc/plans";
+
 export default function PricingSection() {
   return (
     <section>
@@ -8,8 +11,10 @@ export default function PricingSection() {
         </div>
 
         {/* Pricing Cards */}
-        <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
-          Pricing Card
+        <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8 mt-4">
+          {plans.map((plan) => (
+            <PricingCard key={plan.id} {...plan} />
+          ))}
         </div>
       </div>
     </section>
