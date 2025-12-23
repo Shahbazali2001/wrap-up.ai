c030d6ec4629cdcff2ac6744dfff35ebd36e2794
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2025-12-23 22:59:43 +0500

    adding clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+
+// Clerk Provider
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
 
@@ -22,14 +26,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fontSans.variable} font-sans antialiased`}>
-        <div className="relative flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${fontSans.variable} font-sans antialiased`}>
+          <div className="relative flex flex-col min-h-screen">
+            {/* <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn> */}
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
