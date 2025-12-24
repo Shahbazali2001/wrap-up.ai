640b68a99e46370c9e08657bb914ee7cf308fc34
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2025-12-24 23:03:41 +0500

    working on upload form

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { z } from "zod";
 import UploadFormInput from "./upload-form-input";
+
+const schema = z.object({});
+
 export default function UploadForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+  };
+
   return (
-    <div>
-      <UploadFormInput />
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
     </div>
   );
 }
