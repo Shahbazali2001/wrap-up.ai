1b10972fbaafe6fe66a092b3f3a7dd533182f086
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2025-12-24 23:03:05 +0500

    working on input form input

--- components/upload/upload-form-input.tsx
+++ components/upload/upload-form-input.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function UploadFormInput() {
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface UploadFormInputProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function UploadFormInput({ onSubmit }: UploadFormInputProps) {
   return (
-    <form action="" className="flex flex-col gap-6">
-      <input type="file" />
-      <Button>Upload Your PDF</Button>
+    <form action="" onSubmit={onSubmit} className="flex flex-col gap-6">
+      <div className="flex justify-end items-center gap-1.5">
+        <Input
+          className=""
+          type="file"
+          id="file"
+          name="file"
+          accept="application/pdf"
+          required
+        />
+        <Button>Upload Your PDF</Button>
+      </div>
     </form>
   );
 }
