ce008ef7c47350d61e0264c4df9126677c58a5d2
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2026-01-15 21:41:24 +0500

    hydration error removed

--- components/common/bg-gradient.tsx
+++ components/common/bg-gradient.tsx
@@ -1,5 +1,5 @@
 export default function BgGradient() {
   return (
-    <div className="absolute inset-0 bg-gradient-radial from-rose-200 via-rose-500 to-rose-800"></div>
+    <div className="absolute inset-0 -z-10 pointer-events-none bg-gradient-radial from-rose-200 via-rose-500 to-rose-800" />
   );
 }
