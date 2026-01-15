3572b2bfaa38cbf170545c6b3896cb406b82cd80
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2026-01-16 01:15:04 +0500

    clean code

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,36 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+
+// Initialize Uploadthing
+const f = createUploadthing();
+
+// Define the router
+export const ourFileRouter = {
+  pdfUploader: f({
+    pdf: { maxFileCount: 1, maxFileSize: "32MB" },
+  })
+    .middleware(async ({req}) => {
+      // Example: get user info from clerk
+      const user = await currentUser();
+
+      if (!user) {
+        throw new UploadThingError("Unauthorized");
+      }
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("File uploaded successfully:", metadata.userId, file);
+      console.log("File URL:", file.ufsUrl);
+
+      return {
+        userId: metadata.userId,
+        file : file.ufsUrl,
+      };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
