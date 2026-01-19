4e9d0a95a3534a7c6fd7d7443a3b39f4ac8635b8
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2026-01-19 18:18:10 +0500

    updates in the file

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ import CTASection from "@/components/home/cta-section";
 export default function Home() {
   return (
     <div className="relative w-full">
+
+      {/* Background gradient */}
+
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
