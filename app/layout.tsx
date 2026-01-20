a8e53a1a9be2954a8403dd87a39686ebbabcc8fa
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2026-01-21 00:39:33 +0500

    Toast added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./globals.css";
 
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
+import { Toaster } from "sonner";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
