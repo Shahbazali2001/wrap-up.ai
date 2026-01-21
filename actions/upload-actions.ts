f89ef7b98be962a081fbcd1d674ca09e33a2813f
Shahbaz Ali <115034394+Shahbazali2001@users.noreply.github.com>
2026-01-22 01:20:30 +0500

    File for server actions

---
+++ actions/upload-actions.ts
@@ -0,0 +1,46 @@
+'user server';
+
+interface ServerData {
+   userId: string;
+    file : {
+        url: string;
+        name: string;
+    }
+}
+
+export async function generatePdfSummary(uploadResponse: ServerData[]) {
+    
+    if (!uploadResponse) {
+        return {
+            success : false,
+            message : "No file uploaded",
+            data : null
+        }
+    }
+
+
+    const { userId, file } = uploadResponse[0];
+    const fileName = file.name;
+    const pdfUrl = file.url;
+
+    if (!userId || !fileName || !pdfUrl) {
+        return {
+            success : false,
+            message : "Invalid file uploaded",
+            data : null
+        }
+    }
+
+    try{
+
+    }catch(error : unknown){
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+        return {
+            success : false,
+            message : errorMessage,
+            data : null
+        }
+    }
+
+
+}
